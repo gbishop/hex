a95ba5c12e5172ae8688a8769a70e0630321fdba
Gary Bishop <gary.bishop.unc@gmail.com>
2025-07-14 15:02:47 -0400

    Tried weighted selection of opponents

--- PPO.py
+++ PPO.py
@@ -1,86 +1,145 @@
-from hexenv import HexEnv, Opponent
+from hexenv import HexEnv
+from hexgame import HexGame, player2
 from sb3_contrib import MaskablePPO
 from sb3_contrib.common.maskable.utils import get_action_masks
+from stable_baselines3.common.utils import set_random_seed
 import numpy as np
 from typing import cast
 import os
 import os.path as osp
-from glob import glob
 import random
+from glob import glob
 
+SEED = 42
 SIZE = 5
-GENERATIONS = 50
-STEPS = 5000
+
 DIR = "MaskablePPO"
 
+NUM_TIMESTEPS = int(1e9)
+WIN_RATE_WINDOW = 1000
+WIN_RATE_THRESHOLD = 0.55
+
+
+class Opponent:
+    """Always player2"""
+
+    def __init__(self):
+        self.model: MaskablePPO | None = None
+        self.model_generation = 0
+        self.model_weights = {0: 0.5}
+
+    def move(self, game: HexGame) -> bool:
+        """Make the opponents move"""
+        if self.model:
+            # transpose the board
+            obs = -game.board.reshape((game.size, game.size)).transpose().flatten()
+            action, _ = self.model.predict(
+                obs, action_masks=obs == 0, deterministic=False
+            )
+            action = int(action)
+            # transpose the action back
+            r, c = game.rc(action)
+            action = game.index(c, r)
+        else:
+            action = random.choice(game.legal_moves())
+
+        win = game.move(action, player2)
+        return win
+
+    def update(self, lose_rate: float):
+        self.model_weights[self.model_generation] = lose_rate
+        model_file_list = sorted(os.listdir(DIR))
+        weights = [self.model_weights.get(i, 0.5) for i in range(len(model_file_list))]
+        choice = random.choices(range(len(model_file_list)), weights=weights, k=1)[0]
+        print(f"{choice=} {weights=}")
+        self.model_generation = choice
+        filename = osp.join(DIR, model_file_list[choice])
+        self.model = MaskablePPO.load(filename)
+
+
+class HexSelfPlayEnv(HexEnv):
+    def __init__(self, size: int, opponent: Opponent, *args, **kwargs):
+        super().__init__(size, *args, **kwargs)
+        self.wins = 0
+        self.games = 0
+        self.generation = 0
+        self.first = True
+        self.opponent = opponent
+
+    def reset(self, *, seed: int | None = None, **kwargs):
+        obs, info = super().reset(seed=seed, **kwargs)
+
+        if not self.first:
+            # let the opponent go first
+            self.opponent.move(self.game)
+            obs = self._get_obs()
+            info = self._get_info()
+
+        self.first = not self.first
+
+        return obs, info
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info = super().step(action)
+
+        if reward != 1:
+            owin = self.opponent.move(self.game)
+            if owin:
+                terminated = True
+                reward = -1
+
+        if terminated:
+            self.games += 1
+            if reward == 1:
+                self.wins += 1
+            if self.games >= WIN_RATE_WINDOW:
+                win_rate = self.wins / self.games
+                if win_rate > WIN_RATE_THRESHOLD:
+                    self.generation += 1
+                    print(
+                        f"win rate = {win_rate*100:.1f}% {self.generation} vs {self.opponent.model_generation}"
+                    )
+                    save_file = os.path.join(DIR, f"history{self.generation:03d}")
+                    model.save(save_file)
+                    self.opponent.update(1 - win_rate)
+
+                    self.wins = 0
+                    self.games = 0
+
+        return obs, reward, terminated, truncated, info
+
+
 if __name__ == "__main__":
+    set_random_seed(SEED)
     os.makedirs(DIR, exist_ok=True)
 
-    env = HexEnv(SIZE, render_mode="human")
+    env = HexSelfPlayEnv(SIZE, Opponent(), render_mode="human")
 
     obs, info = env.reset()
 
     savedModels = sorted(glob(f"{DIR}/*"))
-    print(savedModels)
-    firstGen = 0
-    if len(savedModels) > 1:
+    env.generation = len(savedModels)
+    if env.generation:
         latest = savedModels[-1]
         model = MaskablePPO.load(latest, env=env)
-        env.set_opponent(Opponent(None))
-        firstGen = len(savedModels)
     else:
         model = MaskablePPO("MlpPolicy", env, verbose=0)
-        env.set_opponent(Opponent(None))
 
-    ogen = -1
-    for gen in range(firstGen, firstGen + GENERATIONS):
-        print(f"{gen=} opponent={ogen}")
-        model.learn(total_timesteps=STEPS)
-        name = osp.join(DIR, f"model_{gen:03d}")
-        model.save(name)
+    model.learn(total_timesteps=NUM_TIMESTEPS)
 
-        if len(savedModels) > 0:
-            ogen = random.randint(0, len(savedModels) - 1)
-            opponent = Opponent(MaskablePPO.load(savedModels[ogen]))
-            env.set_opponent(opponent)
-        savedModels.append(name)
+    model.save(osp.join(DIR, "final_model"))
 
-    F = True
-
-    if F:
-        rewards = []
-        reward = 0
-        for i in range(10):
-            done = False
-            env.verbose = True
-            obs, info = env.reset()
-            while not done:
-                action_masks = get_action_masks(env)
-                action, _ = model.predict(
-                    cast(np.ndarray, obs), action_masks=action_masks, deterministic=True
-                )
-                obs, reward, done, _, info = env.step(action)
-            rewards.append(int(reward))
-        print(rewards)
-
-    else:
-        vec_env = model.get_env()
-        assert vec_env
-        obs = vec_env.reset()
-
+    rewards = []
+    reward = 0
+    for i in range(10):
         done = False
-        rewards = []
-
-        while 1 not in rewards or -1 not in rewards:
+        env.verbose = True
+        obs, info = env.reset()
+        while not done:
             action_masks = get_action_masks(env)
             action, _ = model.predict(
                 cast(np.ndarray, obs), action_masks=action_masks, deterministic=True
             )
-            obs, reward, done, info = vec_env.step(action)
-            # vec_env.render("human")
-            # print(info[0]["board"])
-
-            if done:
-                rewards += [int(r) for r in reward]
-
-        print(rewards)
+            obs, reward, done, _, info = env.step(action)
+        rewards.append(int(reward))
+    print(rewards)
--- hexenv.py
+++ hexenv.py
@@ -1,31 +1,6 @@
-from hexgame import HexGame, player1, player2
+from hexgame import HexGame, player1
 import gymnasium as gym
 from gymnasium import spaces
-import random
-
-
-class Opponent:
-    """Always player2"""
-
-    def __init__(self, model):
-        self.model = model
-
-    def move(self, game: HexGame) -> bool:
-        if self.model:
-            # transpose the board
-            obs = -game.board.reshape((game.size, game.size)).transpose().flatten()
-            action, _ = self.model.predict(
-                obs, action_masks=obs == 0, deterministic=False
-            )
-            action = int(action)
-            # transpose the action back
-            r, c = game.rc(action)
-            action = game.index(c, r)
-        else:
-            action = random.choice(game.legal_moves())
-
-        win = game.move(action, player2)
-        return win
 
 
 class HexEnv(gym.Env):
@@ -61,21 +36,6 @@ class HexEnv(gym.Env):
         super().reset(seed=seed, **kwargs)
         self.game = HexGame(self.size)
 
-        # let player2 go first half the time
-        if self.opponent and random.random() < 0.5:
-            # it's move will be random or deterministic
-            self.info["order"] = "second"
-            if random.random() < 0.5:
-                self.log("\nO random")
-                self.game.move(random.choice(self.game.legal_moves()), player2)
-            else:
-                self.log("\nO predicted")
-                self.opponent.move(self.game)
-            self.log(self.game)
-        else:
-            self.info["order"] = "first"
-            self.log("\nfirst")
-
         observation = self._get_obs()
         info = self._get_info()
 
@@ -87,41 +47,12 @@ class HexEnv(gym.Env):
         action = int(action)
         win = self.game.move(action, player1)
 
-        self.log("X\n", self.game)
-
-        reward = -0.01
-        winner = 0
-        if not win:
-            if self.opponent:
-                owin = self.opponent.move(self.game)
-                self.log("O\n", self.game)
-                if owin:
-                    reward = -1
-                    winner = player2
-        else:
-            reward = 1
-            winner = player1
-
-        if winner:
-            self.log(f"{winner=}")
-
+        reward = 1.0 if win else 0.0
         obs = self._get_obs()
         self.info["board"] = str(self.game)
         info = self._get_info()
         truncated = False
-        terminated = winner != 0
-
-        if winner:
-            wins = info["wins"]
-            wins[winner] += 1
-            wins[info["order"]] += 1
-            N = wins[player1] + wins[player2]
-            first = wins["first"]
-            if N % 100 == 0:
-                print(f"player 1 win = {100 * wins[player1] / N:.1f}%")
-                print(f"first win = {100 * first / N:.1f}%")
-                for key in wins:
-                    wins[key] = 0
+        terminated = win
 
         return obs, reward, terminated, truncated, info
 
@@ -133,10 +64,6 @@ class HexEnv(gym.Env):
     def action_masks(self):
         """Mask off illegal moves"""
         return self.game.board == 0
-
-    def set_opponent(self, opponent):
-        """Install a new opponent"""
-        self.opponent = opponent
 
     def log(self, *args, **kwargs):
         if self.verbose:
